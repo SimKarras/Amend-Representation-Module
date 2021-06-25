2383820070f73c380967844f7e34040cf49e61d6
Jiawei Shi <57309899+JiaweiShiCV@users.noreply.github.com>
2021-06-25 10:36:51 +0800

    Create test_raf-db.py

---
+++ src/test_raf-db.py
@@ -0,0 +1,109 @@
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import torch.utils.data as data
+from torchvision import transforms
+import cv2
+import pandas as pd
+import os, torch
+import image_utils
+import argparse, random
+import Networks
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--raf_path', type=str, default='./datasets/raf-basic/', help='Raf-DB dataset path.')
+    parser.add_argument('-c', '--checkpoint', type=str, default=None, help='Pytorch checkpoint file path')
+    parser.add_argument('--workers', default=4, type=int, help='Number of data loading workers (default: 4)')
+    return parser.parse_args()
+    
+class RafDataSet(data.Dataset):
+    def __init__(self, raf_path, phase, transform=None, basic_aug=False):
+        self.phase = phase
+        self.transform = transform
+        self.raf_path = raf_path
+
+        NAME_COLUMN = 0
+        LABEL_COLUMN = 1
+        df = pd.read_csv(os.path.join(self.raf_path, 'EmoLabel/list_patition_label.txt'), sep=' ', header=None)
+        if phase == 'train':
+            dataset = df[df[NAME_COLUMN].str.startswith('train')]
+        else:
+            dataset = df[df[NAME_COLUMN].str.startswith('test')]
+        file_names = dataset.iloc[:, NAME_COLUMN].values
+        self.label = dataset.iloc[:, LABEL_COLUMN].values - 1   # 0:Surprise, 1:Fear, 2:Disgust, 3:Happiness, 4:Sadness, 5:Anger, 6:Neutral
+        
+        self.file_paths = []
+        # use raf-db aligned images for training/testing
+        for f in file_names:
+            f = f.split(".")[0]
+            f = f +"_aligned.jpg"
+            path = os.path.join(self.raf_path, 'Image/aligned', f)
+            self.file_paths.append(path)
+        
+        self.basic_aug = basic_aug
+        self.aug_func = [image_utils.flip_image, image_utils.add_gaussian_noise]
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        path = self.file_paths[idx]
+        image = cv2.imread(path)
+        image = image[:, :, ::-1]            # BGR to RGB
+        label = self.label[idx]
+        if self.phase == 'train':
+            if self.basic_aug and random.uniform(0, 1) > 0.5:
+                index = random.randint(0, 1)
+                image = self.aug_func[index](image)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        return image, label, idx
+
+
+def test():
+    args = parse_args()
+    model = Networks.ResNet18_ARM___RAF()
+    print("batch_size:", args.batch_size)
+
+    print("Loading pretrained weights...", args.checkpoint)
+    checkpoint = torch.load(args.checkpoint)
+    model.load_state_dict(checkpoint["model_state_dict"], strict=False)
+
+    data_transforms_test = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    test_dataset = RafDataSet(args.raf_path, phase='test', transform=data_transforms_test)
+    test_size = test_dataset.__len__()
+    print('Test set size:', test_size)
+
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                             batch_size=args.batch_size,
+                                             num_workers=args.workers,
+                                             shuffle=False,
+                                             pin_memory=True)
+
+    model = model.cuda()
+
+    with torch.no_grad():
+        bingo_cnt = 0
+        model.eval()
+        for batch_i, (imgs, targets, _) in enumerate(test_loader):
+            outputs, _ = model(imgs.cuda())
+            targets = targets.cuda()
+            _, predicts = torch.max(outputs, 1)
+            correct_or_not = torch.eq(predicts, targets)
+            bingo_cnt += correct_or_not.sum().cpu()
+
+        acc = bingo_cnt.float() / float(test_size)
+        acc = np.around(acc.numpy(), 4)
+        print(f"Test accuracy: {acc:.4f}.")
+
+
+if __name__ == "__main__":                    
+    test()
